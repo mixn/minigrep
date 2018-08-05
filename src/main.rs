645d6fa640c4a3768fe7917188221b31c36aec6b
mixn <sutanovac.milos@gmail.com>
2018-08-05 16:48:58 +0200

    ♻️ Embraces struct + refactors

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,34 @@ use std::fs::File;
 // Contains various useful traits for doing I/O, including file I/O
 use std::io::prelude::*;
 
+// Use a struct to better associate query 
+// with filename and be more explicit
+struct Config {
+    query: String,
+    filename: String
+}
+
+fn parse_config(args: &[String]) -> Config {
+    // Inefficient but easiest, since it avoids lifetimes
+    let query = args[1].clone();
+    let filename = args[2].clone();
+
+    Config {
+        query,
+        filename
+    }
+}
+
 fn main() {
     // `collect` will turn an iterator into
     // a collection, in this case a Vector
     let args: Vec<String> = env::args().collect();
-    // let query = &args[1];
-    let filename = &args[2];
+    let config = parse_config(&args);
+
+    println!("Searching for {} in {}", config.query, config.filename);
+
     // `expect` is needed/important, otherwise `read_to_string` will fail
-    let mut f = File::open(filename).expect("File not found.");
+    let mut f = File::open(config.filename).expect("File not found.");
     let mut content = String::new();
 
     f.read_to_string(&mut content).expect("Something went wrong reading the file.");
