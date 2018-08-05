e7b4522e4436a7c75d46278ec361862508aaffe3
mixn <sutanovac.milos@gmail.com>
2018-08-05 15:58:06 +0200

    🚧 Enables reading from a file

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,20 @@
 use std::env;
+// For handling files
+use std::fs::File;
+// Contains various useful traits for doing I/O, including file I/O
+use std::io::prelude::*;
 
 fn main() {
     // `collect` will turn an iterator into
     // a collection, in this case a Vector
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+    // let query = &args[1];
+    let filename = &args[2];
+    // `expect` is needed/important, otherwise `read_to_string` will fail
+    let mut f = File::open(filename).expect("File not found.");
+    let mut content = String::new();
+
+    f.read_to_string(&mut content).expect("Something went wrong reading the file.");
+
+    println!("{}", content);
 }
